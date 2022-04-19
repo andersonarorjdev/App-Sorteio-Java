04f792c55ecc7c957853359b6131a079ecd21d32
Andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2022-04-19 21:53:16 +0000

    changed onClick to setOnClickListener in button trigger.

--- app/src/main/java/com/example/andersonjunior/MainActivity.java
+++ app/src/main/java/com/example/andersonjunior/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import java.util.Random;
 
@@ -15,16 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        executeInStart();
     }
 
-    public void realizarSorteio(View view){
-        TextView campoResultado = findViewById(R.id.resultado);
-        Random rand = new Random();
-        this.numberSorted = rand.nextInt(11);
+    //metodo abstraído para o setOnClickListener para nao depender do onClick que já é deprecated.
+    public void executeInStart(){
+        Button button = (Button) findViewById(R.id.button);
 
-        //entender o por que daquele erro ao passar uma string para esse campo.
-        campoResultado.setText(""+this.numberSorted);
-        campoResultado.getCurrentTextColor();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView campoResultado = findViewById(R.id.resultado);
+                Random rand = new Random();
+                numberSorted = rand.nextInt(11);
+
+                //entender o por que daquele erro ao passar uma string para esse campo.
+                campoResultado.setText(""+numberSorted);
+                campoResultado.getCurrentTextColor();
+            }
+        });
     }
 
     public void mudarParaOutraTela(View view){
